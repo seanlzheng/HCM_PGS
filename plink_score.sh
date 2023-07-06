6a122b6a87300f365b9c59611dc4d76b54af4219
seanlzheng <53749449+seanlzheng@users.noreply.github.com>
2023-07-06 10:00:31 +0100

    Update plink_score.sh

--- plink_score.sh
+++ plink_score.sh
@@ -1,12 +1,3 @@
-#PBS -lselect=1:ncpus=32:mem=124gb
-#PBS -lwalltime=72:00:00
-
-$PBS_O_WORKDIR
-
-module load anaconda3/personal
-source activate prs
-
-module load plink
 plink \
 --bfile /rds/general/project/lms-ware-analysis/live/sean/prs/HCM/target/UKBB/UKBB_GWAS_MERGE_INCLUDING_RELATED.QC \
 --score /rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/HCM/output/posterior_snp_effect_estimates/hcm.mtag.exc.mybpc3.1kg.inc.related.merge.txt 2 4 6 \
