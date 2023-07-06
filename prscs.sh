5f1120ba4bdb818512b40455fb286f5c2edad03e
seanlzheng <53749449+seanlzheng@users.noreply.github.com>
2023-07-06 10:00:53 +0100

    Update prscs.sh

--- prscs.sh
+++ prscs.sh
@@ -1,11 +1,3 @@
-#PBS -lselect=1:ncpus=32:mem=124gb
-#PBS -lwalltime=72:00:00
-
-$PBS_O_WORKDIR
-
-module load anaconda3/personal
-source activate prs
-
 python /rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/PRScs.py \
 --ref_dir=/rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/1kg_reference/ldblk_1kg_eur \
 --bim_prefix=/rds/general/project/lms-ware-analysis/live/sean/prs/HCM/target/UKBB/UKBB_GWAS_MERGE_INCLUDING_RELATED.QC \
